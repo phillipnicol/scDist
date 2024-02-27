60e5c58ba508e94346af5ad86c1b905e190c29a6
Phillip Nicol <43178367+phillipnicol@users.noreply.github.com>
2024-02-27 11:20:54 -0500

    Changing things

--- analysis/count_deg_blish/code/upsample.R
+++ analysis/count_deg_blish/code/upsample.R
@@ -2,13 +2,17 @@
 library(Seurat)
 library(scDist)
 library(SingleCellExperiment)
+library(Matrix)
+library(muscat)
 
-Sco <- readRDS("../../data/blish_covid.seu.rds")
+Y.sct <- readRDS("../../data/blish_SCT_counts.RDS")
+Y.counts <- readMM("../../data/blish_raw_counts.txt")
+meta.data <- readRDS("../../data/blish_meta.RDS")
 
-upsample <- function(Sco,ct.size) {
+upsample <- function(meta.data,ct.size) {
   ixs <- c()
-  for(ct in unique(Sco$cell.type.coarse)) {
-    ix <- sample(which(Sco$cell.type.coarse == ct), size=ct.size,
+  for(ct in unique(meta.data$cell.type.coarse)) {
+    ix <- sample(which(meta.data$cell.type.coarse == ct), size=ct.size,
                  replace=TRUE)
     ixs <- c(ixs,ix)
   }
@@ -16,20 +20,18 @@ upsample <- function(Sco,ct.size) {
 }
 
 
-muscat_deg <- function(Sco.sub) {
-  sce.sub <- SingleCellExperiment(counts=Sco@assays$RNA@counts[,ixs],
-                                  colData=meta.data[ixs,])
+muscat_deg <- function(sce.sub) {
   sce.sub <- prepSCE(sce.sub,
-                     kid = "cell.type.coarse", # subpopulation assignments
-                     gid = "Status",  # group IDs (ctrl/stim)
-                     sid = "Donor",   # sample IDs (ctrl/stim.1234)
+                     kid = "cluster", # subpopulation assignments
+                     gid = "response",  # group IDs (ctrl/stim)
+                     sid = "sample",   # sample IDs (ctrl/stim.1234)
                      drop = TRUE)
 
   pb <- aggregateData(sce.sub,
                       assay = "counts", fun = "sum",
                       by = c("cluster_id", "sample_id"))
 
-  res <- pbDS(pb, verbose = FALSE)
+  res <- pbDS(pb, verbose = FALSE, min_cells=0)
 
   tbl <- res$table[[1]]
   # filter FDR < 5%, abs(logFC) > 1 & sort by adj. p-value
@@ -46,43 +48,86 @@ muscat_deg <- function(Sco.sub) {
 }
 
 
-meta.data <- data.frame(response=Sco$Status,
-                        sample=Sco$Donor,
-                        cluster=Sco$cell.type.coarse)
-out.full <- scDist(Sco@assays$SCT@scale.data[Sco@assays$SCT@var.features,],
+meta.data <- meta.data[,c("Status", "Donor", "cell.type.coarse")]
+colnames(meta.data) <- c("response", "sample", "cluster")
+
+out.full <- scDist(Y.sct,
                    meta.data,
                    fixed.effects="response",
                    random.effects="sample",
                    clusters="cluster")
 
+sce <- SingleCellExperiment(assay=list(counts=Y.counts), colData=meta.data)
+
 
 
 set.seed(1)
 reps <- 20
-nc <- length(table(Sco$cell.type.coarse))
-ct.size <- 10^{2:5}
+nc <- length(table(meta.data$cluster))
+cts <- unique(meta.data$cluster)
+ct.size <- round(10^{seq(log10(200), 4, length.out=10)})
 res <- array(dim=c(nc,length(ct.size),reps,2))
+time <- array(dim=c(length(ct.size), reps,2))
 
 for(i in 1:length(ct.size)) {
   for(j in 1:reps) {
+    cat(i, " ", j, "\n")
     ixs <- upsample(Sco,ct.size=ct.size[i])
     meta.sub <- meta.data[ixs,]
-    Y.sub <- Sco@assays$SCT@scale.data[,ixs]
-    Y.sub <- Y.sub[Sco@assays$SCT@var.features,]
+    Y.sub <- Y.sct[,ixs]
 
+    start <- Sys.time()
     out.sub <- scDist(Y.sub,
                       meta.sub,
                       fixed.effects="response",
                       random.effects="sample",
                       clusters="cluster")
+    end <- Sys.time()
+    time[i,j,1] <- difftime(end,start,units="secs")
 
     res[,i,j,1] <- out.sub$results$Dist.
 
-    sce.sub <- SingleCellExperiment(assays=list(counts=counts.new),
-                                    colData=coldata)
-    coldata <- DataFrame(meta.sub)
-
-    df <- muscat_deg(Sco, ixs)
+    sce.sub <- sce[,ixs]
+    start <- Sys.time()
+    df <- muscat_deg(sce.sub)
+    end <- Sys.time()
+    time[i,j,2] <- difftime(end,start,units="secs")
     res[,i,j,2] <- df$`#DS`
   }
 }
+
+saveRDS(res, "../data/upsample_res.RDS")
+saveRDS(time, "../data/upsample_time.RDS")
+
+res <- readRDS("../data/upsample_res.RDS")
+time <- readRDS("../data/upsample_time.RDS")
+
+library(tidyverse)
+
+#plot of cell types paths
+
+df <- reshape2::melt(res)
+df <- df |> group_by(Var1, Var2, Var4) |> summarize(mean = mean(value))
+
+df$Var1 <- cts[df$Var1]
+df$Var2 <- ct.size[df$Var2]
+
+p <- df |> ggplot(aes(x=Var2,y=mean,color=Var1)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~Var4) +
+  theme_bw()
+
+ggsave(plot=p, filename="../plots/upsample_ct_path.png")
+
+## Plot of correlation with ranking on full data
+df <- reshape2::melt(res)
+
+scDist.cor <- apply(res[,,,1], c(2,3), function(x) {
+  cor(out.full$results$Dist., x)
+})
+
+deg.cor <- apply(res[,,,2], c(2,3), function(x) {
+  cor(out.full$results$Dist., x)
+})
+
