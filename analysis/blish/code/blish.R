b3da76b1177b32e6d32a2aa1b4af5e13ccdee567
Phillip Nicol <43178367+phillipnicol@users.noreply.github.com>
2024-10-21 20:06:14 -0400

    Updates for poster

--- analysis/blish/code/blish.R
+++ analysis/blish/code/blish.R
@@ -19,3 +19,15 @@ out2 <- condition_v_celltype_distance(normalized_counts = Sco@assays$SCT@scale.d
                                       random.effects = "Donor",
                                       clusters = "cell.type.coarse")
 saveRDS(out2$D, "../data/celltype_v_condition.RDS")
+
+
+p.dist.plot <- scDist::DistPlot(out, return.plot = TRUE)
+p.fdr.plot <- scDist::FDRDistPlot(out)$plot
+p.new.plot <- scDist::distGenes(out,"CD14 Monocyte") + ggtitle("CD14 Monocytes")
+
+p <- ggarrange(ggarrange(p.dist.plot, p.fdr.plot, nrow=1,ncol=2),
+          p.new.plot, nrow=2, ncol=1, heights=c(1.5,1))
+
+ggsave(p, filename="../plots/blish_example.png",
+       width=8.64, height=7.1)
+
